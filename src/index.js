c5c55157afa222565bd44a7a2fc07a72536cd52b
Jhok20 <jmonesruiz20@gmail.com>
2021-12-05 19:01:51 -0300

    Added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/rest-countries">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
